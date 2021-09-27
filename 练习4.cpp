32a3db38b667991befb7e8c1a679c8af1ee74f9e
Passerby2022 <90971723+Passerby2022@users.noreply.github.com>
2021-09-27 19:27:49 +0800

    the fifth day

---
+++ 练习4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int width, length;
+	float height, profit;
+	printf("Width: %d Length: %d\n", width, length);
+	printf("Height: %f Profit: %f\n", height, profit);
+	return 0;
+ } 
