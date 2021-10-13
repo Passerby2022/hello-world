58afb6ac70dcaf3433c6e1d31e16dcc65f259aeb
Passerby2022 <90971723+Passerby2022@users.noreply.github.com>
2021-10-13 12:56:32 +0800

    the twenty-ninth day

--- 练习3.cpp
+++ 练习3.cpp
@@ -1,16 +1,34 @@
 #include <stdio.h>
- 
- int main (void)
- {
- 	int height = 8; 
- 	int length = 10;
- 	int width = 12;
- 	int volume = height * length * width;
- 	
-    printf("Dimensions: %dx%dx%d\n", length, width, height);
- 	printf("Volume (cubic inches): %d\n", volume);
- 	printf("Dimensional  (volume + 165)/166) (pounds): %d\n", (volume + 165)/166);
- 	
- 	return 0;
- 	
- }
+
+int main (void)
+{
+    int area_code;
+
+    printf("Enter a number: ");
+    scanf("%3d", &area_code);
+
+    switch (area_code) {
+        case 229: printf("Albany");
+        break;
+        case 404: printf("Atlanta");
+                break;
+        case 470: printf("Atlanta");
+                  break;
+        case 478: printf("Macon");
+                 break;
+        case 678: printf("Atlanta");
+                  break;
+        case 706: printf("Columbus");
+                  break;
+        case 762: printf("Columbus");
+                  break;
+        case 770: printf("Atlanta");
+                  break;
+        case 912: printf("Savannah");
+                  break;
+        default: printf("Area code not recognized");
+                 break;
+    }
+
+    return 0;
+}
