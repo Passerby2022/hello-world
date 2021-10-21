58706608ddf79bba55f812284fb0af833adde5d7
Passerby2022 <90971723+Passerby2022@users.noreply.github.com>
2021-10-21 21:49:57 +0800

    the forty-first day

---
+++ 练习6.1plus.cpp
@@ -0,0 +1,53 @@
+/* Displays the maximun non negative number that has been entered */
+
+#include <stdio.h>
+
+int main (void)
+{
+    float num, max;
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    for(; num > max; ){
+        if (num > max)
+            max = num;
+        printf ("Enter a number: ");
+        scanf("%f", &num);
+    }
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    for(; num > max; ){
+        if (num > max)
+            max = num;
+        printf ("Enter a number: ");
+        scanf("%f", &num);
+    }
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    for(; num > max; ){
+        if (num > max)
+            max = num;
+        printf ("Enter a number: ");
+        scanf("%f", &num);
+    }
+
+    printf("Enter a number: ");
+    scanf("%f", &num);
+
+    for(; num > max; ){
+        if (num > max)
+            max = num;
+        printf ("Enter a number: ");
+        scanf("%f", &num);
+    }
+
+
+    printf("The largest number entered was %.2f", max);
+
+    return 0;
+}
