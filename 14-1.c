bc570e025b0f662de8d310b32810c51ae5e2e69c
Passerby2022 <90971723+Passerby2022@users.noreply.github.com>
2022-05-20 20:19:43 +0800

    Add files via upload

---
+++ 14-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    FILE *fp, *sfp;
+    char filename[30], filename1[30];
+    char str[30];
+    printf("please input filename\n");
+    scanf("%s", filename);
+    printf("please input filename\n");
+    scanf("%s", filename1);
+
+    if((fp = fopen(filename, "r")) == NULL){
+        printf("cannot open file\n");
+        exit(0);
+    }
+    if((sfp = fopen(filename1, "w")) == NULL){
+        printf("cannot open file\n");
+        exit(0);
+    }
+
+    fgets(str, sizeof(str), fp);
+    fputs(str, sfp);
+    fclose(fp);
+    fclose(sfp);
+    return 0;
+}
